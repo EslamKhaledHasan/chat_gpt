a271c692ce018329edea4e44203c0dc03190e701
eslaamm66 <you@example.com>
2023-01-25 19:03:24 +0200

    add container

--- lib/main.dart
+++ lib/main.dart
@@ -150,6 +150,57 @@ class ChatMessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
+   Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(16),
+      color: chatMessageType == ChatMessageType.bot
+          ? botBackgroundColor
+          : backgroundColor,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          chatMessageType == ChatMessageType.bot
+              ? Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    backgroundColor: Color.fromARGB(255, 165, 192, 66),
+                    child: Image.asset(
+                      'assets/ee.png',
+                      color: Colors.white,
+                      scale: 1.5,
+                    ),
+                  ),
+                )
+              : Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: const CircleAvatar(
+                    child: Icon(
+                      Icons.person_outline,
+                    ),
+                  ),
+                ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(9),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(9)),
+                  ),
+                  child: Text(
+                    text,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    )
 }
 }
