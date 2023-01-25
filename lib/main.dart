f970091723d5b11de2664fd479757b93ee2da900
eslaamm66 <you@example.com>
2023-01-25 18:02:50 +0200

    const colors and chatpage class

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: ChatPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+const backgroundColor = Color.fromARGB(255, 66, 68, 68);
+const botBackgroundColor = Color.fromARGB(255, 4, 116, 124);
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override 
+  Widget build(BuildContext context){
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          
+        )
+        
+        ,)
+    )
   }
 }
